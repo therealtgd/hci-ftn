1b6d1526131de932f8b3a77bd7171c4bcb0f416d
ikkjo <ilija.kalinic@gmail.com>
2022-06-09 18:26:11 +0200

    Adds Ride route showing on client map view, Adds SimpleWaypoints to Ride

--- isRail/isRail/Models/App.cs
+++ isRail/isRail/Models/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BingMapsRESTToolkit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,9 @@ namespace isRail.Models
 
         public void InitializeApp()
         {
+            SimpleWaypoint[] ride1_waypoints = { new SimpleWaypoint(45.249630, 19.396850),
+                                                 new SimpleWaypoint(45.365810, 20.403580),
+                                                 new SimpleWaypoint(46.102779, 19.670427) };
             Ride ride1 = new Ride(
                 "Lasta",
                 "Novi Sad",
@@ -27,7 +31,13 @@ namespace isRail.Models
                 new List<string> { "Backa Palanka", "Zrenjanin", "Subotica" },
                 DateTime.Now.AddHours(0.2),
                 DateTime.Now.AddHours(0.5),
-                1500);
+                1500,
+                new BingMapsRESTToolkit.SimpleWaypoint(45.265571, 19.829366),
+                new BingMapsRESTToolkit.SimpleWaypoint(44.808510, 20.455799),
+                new List<SimpleWaypoint>(ride1_waypoints)
+                );
+
+            SimpleWaypoint[] ride2_waypoints = { new SimpleWaypoint(45.365810, 20.403580) };
             Ride ride2 = new Ride(
             "Jastreb",
             "Subotica",
@@ -35,7 +45,14 @@ namespace isRail.Models
             new List<string> { "Zrenjanin" },
             DateTime.Now.AddHours(1),
             DateTime.Now.AddHours(2),
-            2000);
+            2000,
+            new BingMapsRESTToolkit.SimpleWaypoint(46.102779, 19.670427),
+            new BingMapsRESTToolkit.SimpleWaypoint(44.808510, 20.455799),
+            new List<SimpleWaypoint>(ride2_waypoints));
+
+            SimpleWaypoint[] ride3_waypoints = { new SimpleWaypoint(45.249630, 19.396850),
+                                                 new SimpleWaypoint(45.365810, 20.403580),
+                                                 new SimpleWaypoint(46.102779, 19.670427) };
             Ride ride3 = new Ride(
             "Orao",
             "Niš",
@@ -43,7 +60,10 @@ namespace isRail.Models
             new List<string> { "Backa Palanka", "Zrenjanin", "Subotica" },
             DateTime.Now.AddDays(1).AddHours(2),
             DateTime.Now.AddDays(1).AddHours(3),
-            3000);
+            3000,
+            new BingMapsRESTToolkit.SimpleWaypoint(43.316257, 21.877323),
+            new BingMapsRESTToolkit.SimpleWaypoint(44.982293, 19.613703),
+            new List<SimpleWaypoint>(ride3_waypoints));
 
             Rides = new List<Ride>();
 
