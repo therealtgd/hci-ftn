7ec37f1d2985fae8ce9726b1b24a16c51e36c8f8
Nikola Damjanovic <cipinikolica@gmail.com>
2022-06-10 18:25:33 +0200

    Added menu items keybindings and all views related to manager

---
+++ isRail/isRail/ViewModels/ManagerEditRideBasesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isRail.ViewModels
+{
+    public class ManagerEditRideBasesViewModel : ViewModelBase
+    {
+        public Models.App App { get; set; }
+
+        public ManagerEditRideBasesViewModel(Models.App app)
+        {
+            App = app;
+        }
+    }
+}
---
+++ isRail/isRail/ViewModels/ManagerReportMonthlyViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isRail.ViewModels
+{
+    public class ManagerReportMonthlyViewModel : ViewModelBase
+    {
+        public Models.App App { get; set; }
+
+        public ManagerReportMonthlyViewModel(Models.App app)
+        {
+            App = app;
+        }
+    }
+}
