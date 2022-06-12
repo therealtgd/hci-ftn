0be08895fffb0cbb13a187a8d65fc6ade9fc0749
ikkjo <ilija.kalinic@gmail.com>
2022-06-12 21:04:02 +0200

    Updates ManagerEditRideBases, adds edit ride bases commands

---
+++ isRail/isRail/Commands/AddRideBaseCommand.cs
@@ -0,0 +1,32 @@
+﻿using isRail.ViewModels;
+using isRail.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isRail.Commands
+{
+    internal class AddRideBaseCommand : CommandBase
+    {
+        private ManagerEditRideBasesViewModel _managerEditRideBasesViewModel;
+        public static event Action AddedRideBaseEvent;
+
+        public AddRideBaseCommand(ManagerEditRideBasesViewModel managerEditRideBasesViewModel)
+        {
+            _managerEditRideBasesViewModel = managerEditRideBasesViewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            MessageBoxInputCustom messageBox = new MessageBoxInputCustom("Dodaj liniju", _managerEditRideBasesViewModel);
+            messageBox.ShowDialog();
+            bool? result = messageBox.DialogResult;
+            if (result.Value)
+            {
+                    AddedRideBaseEvent?.Invoke();
+            }
+        }
+    }
+}
--- isRail/isRail/ViewModels/ManagerEditRideBasesViewModel.cs
+++ isRail/isRail/ViewModels/ManagerEditRideBasesViewModel.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using isRail.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace isRail.ViewModels
 {
@@ -10,9 +14,28 @@ namespace isRail.ViewModels
     {
         public Models.App App { get; set; }
 
+        public ObservableCollection<RideBase> RideBases { get; set; }
+
+        public ICollectionView RideBasesCollectionView { get; set; }
+
         public ManagerEditRideBasesViewModel(Models.App app)
         {
             App = app;
+
+            RideBases = new ObservableCollection<RideBase>();
+
+            foreach (RideBase rideBase in app.RidesMap.Keys)
+                RideBases.Add(new RideBase(rideBase.Id, rideBase.To, rideBase.From, rideBase.Stations));
+
+            RideBasesCollectionView = CollectionViewSource.GetDefaultView(RideBases);
+
+            //SaveTrainChangesCommand = new SaveTrainChangesCommand(this);
+            //DiscardTrainChangesCommand = new DiscardTrainChangesCommand(this);
+            //AddTrainCommand = new AddTrainCommand(this);
+
+            //DiscardTrainChangesCommand.DiscardChangesEvent += OnDiscardChanges;
+            //AddTrainCommand.AddedTrainEvent += OnDiscardChanges;
+
         }
     }
 }
